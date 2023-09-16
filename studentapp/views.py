80596ac24389ec42d07861da955b315240d830f0
Hariesh <hariesh@iamneo.ai>
2023-09-16 17:15:41 +0530

    best lang used

--- studentapp/views.py
+++ studentapp/views.py
@@ -9,7 +9,16 @@ from usersapp.models import *
 def landing_page(request):
 	user_obj = User.objects.get(id=request.user.id)
 	lang_data = language_wise.objects.filter(email=user_obj)
-	context = {'lang_data': lang_data}
+
+	max_entry = max(lang_data, key=lambda x: x.attended())
+	
+	max_questions_attended = max_entry.attended
+	max_lang_used = max_entry.language_used
+	
+	context = {
+		'lang_data': lang_data,
+		'max_lang_used': max_lang_used
+	}
 	return render(request,'studentapp/dashboard.html',context=context)
 
 
